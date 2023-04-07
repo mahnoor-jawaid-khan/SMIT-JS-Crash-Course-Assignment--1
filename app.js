9e78ea9f0e711eedf77935c42d3fe0c4fa48d2ac
mahnoor-jawaid-khan <mahnoorkhan1034@gmail.com>
2023-04-07 19:54:33 +0500

    JS ASSIGNMENT_1

---
+++ app.js
@@ -0,0 +1,112 @@
+// ---------------------Task 1--------------------------
+
+// let value1 = +prompt("Enter First Number");
+// let value2 = +prompt("Enter second Number");
+
+// if( value1 > value2 ){
+//     console.log(value1 + " is a larger number");
+// }
+// else{
+//     console.log(value2 + " is a larger number.");
+// }
+
+// ---------------------Task 2--------------------------
+
+// let number = +prompt("Enter a Number");
+
+// if( number >= 0 ){
+//     console.log("The sign is +.");
+// }
+// else{
+//     console.log("The sign is -.");
+// }
+
+// ---------------------Task 3--------------------------
+
+// let num1 = +prompt("Enter a Number");
+// let num2 = +prompt("Enter a Number");
+// let num3 = +prompt("Enter a Number");
+// let num4 = +prompt("Enter a Number");
+// let num5 = +prompt("Enter a Number");
+// let largest;
+// if(num1 >= num2 && num1 >= num3 && num1 >= num4 && num1 >= num5){
+//     largest = num1;
+// }
+// else if (num2 >= num1 && num2 >= num3 && num2 >= num4 && num2 >= num5){
+//     largest = num2;
+// }
+// else if (num3 >= num1 && num3 >= num2 && num3 >= num4 && num3 >= num5){
+//     largest = num3;
+// }
+// else if (num4 >= num1 && num4 >= num2 && num4 >= num3 && num4 >= num5){
+//     largest = num4;
+// }
+// else if (num5 >= num1 && num5 >= num2 && num5 >= num3 && num5 >= num4){
+//     largest = num5;
+// }
+
+
+// console.log("The largest number is: " + largest);
+
+// ---------------------Task 4--------------------------
+
+// for (var i = 0; i <= 15; i++)
+// {
+//     if (i === 0){
+//         console.log(i + " is a Even number: ");
+//     }
+//     else if (i % 2 === 0){
+//         console.log(i + " is a odd number: ");
+// }
+//     else{
+//         console.log(i + " is a odd number: ");
+//     }
+// }
+
+// ---------------------Task 5--------------------------
+
+// let std_marks = +prompt("Enter your number");
+    
+//     if (std_marks < 60) {
+//         console.log("Grade: F");
+//     }
+//     else if (std_marks < 70) {
+//         console.log("Grade: D");
+//     }
+//     else if (std_marks < 80) {
+//         console.log("Grade: C");
+//     } 
+//     else if (std_marks< 90) {
+//         console.log("Grade: B");
+//     } 
+//     else if (std_marks < 100) {
+//         console.log("Grade: A");
+//     }
+
+
+// ---------------------Task 6--------------------------
+
+// for (var m = 1; m <= 100; m++) 
+// {
+//     if (m % 3 === 0 && m % 5 === 0) {
+//         console.log(m + " FizzBuzz");
+//     }
+//     else if (m % 3 === 0) {
+//         console.log(m + " Fizz");
+//     }
+//     else {
+//         console.log(m + " Buzz");
+//     }
+// }
+
+
+// ---------------------Task 7--------------------------
+
+// for (var outerLoop = 1; outerLoop <= 5; outerLoop++)
+//     {
+//         for (var innerLoop = 1; innerLoop <= outerLoop; innerLoop++ )
+//         {
+//            document.write("*");
+//         }
+//         document.write("<br />");
+//     }
